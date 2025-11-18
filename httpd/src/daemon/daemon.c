9a9709a05e849478e6bcbac211bbe3f2aa1ebd4d
Maxthur <maxime.robert@epita.fr>
2025-11-18 22:14:31 +0100

    daemon logic is now supported

--- httpd/src/daemon/daemon.c
+++ httpd/src/daemon/daemon.c
@@ -1,9 +1,13 @@
 #include "daemon.h"
 
 #include <stdio.h>
+#include <string.h>
+#include <sys/signal.h>
+#include <sys/types.h>
 #include <unistd.h>
 
 #include "../config/config.h"
+#include "../logger/logger.h"
 
 static int add_pid(const struct config *config, pid_t pid)
 {
@@ -12,24 +16,93 @@ static int add_pid(const struct config *config, pid_t pid)
     if (!pid_file)
         return 1;
 
-    fprintf(pid_file, "%d\n", pid);
+    // Write PID to file
+    fprintf(pid_file, "%d", pid);
     fclose(pid_file);
     return 0;
 }
 
-int start_daemon(const struct config *config)
+static bool pid_exists(const struct config *config)
 {
-    printf("Starting daemon with PID file: %s\n", config->pid_file);
+    FILE *pid_file = fopen(config->pid_file, "r");
+    if (!pid_file)
+        return false;
+
+    pid_t existing_pid;
+    while (fscanf(pid_file, "%d", &existing_pid) == 1)
+    {
+        // Send signal 0 to check if process exists
+        if (kill(existing_pid, 0) == 0)
+        {
+            fclose(pid_file);
+            return true;
+        }
+    }
+
+    fclose(pid_file);
+    return false;
+}
+
+int start_daemon(struct config *config)
+{
+    char msg[256];
+    if (pid_exists(config))
+    {
+        sprintf(msg, "A daemon is already running with same PID file (%s).\n",
+                config->pid_file);
+        logger_log(config, msg);
+        return 1;
+    }
+
+    sprintf(msg, "Starting daemon with PID file: %s\n", config->pid_file);
+    logger_log(config, msg);
 
     pid_t pid = fork();
     if (pid < 0)
         return 1;
     if (pid > 0)
+    {
+        logger_destroy(); // Close log files for parent process
         _exit(0); // Terminate parent process
+    }
 
-    // Child process
+    // Add child PID to PID file
     if (add_pid(config, getpid()) != 0)
         return 1;
 
+    // Add log file if needed
+    if (config->log && !config->log_file)
+        config->log_file = strdup("HTTPd.log");
+
     return 0;
+}
+
+int stop_daemon(struct config *config)
+{
+    FILE *pid_file = fopen(config->pid_file, "r");
+    if (!pid_file)
+    {
+        printf("PID file not found: %s.\n", config->pid_file);
+        return 0;
+    }
+
+    pid_t existing_pid;
+    while (fscanf(pid_file, "%d", &existing_pid) == 1)
+    {
+        // Kill the process
+        if (kill(existing_pid, SIGINT) == 0)
+            break;
+    }
+
+    // Close PID file and delete it
+    fclose(pid_file);
+    remove(config->pid_file);
+
+    return 0;
+}
+
+int restart_daemon(struct config *config)
+{
+    stop_daemon(config);
+    return start_daemon(config);
 }
--- httpd/src/daemon/daemon.h
+++ httpd/src/daemon/daemon.h
@@ -5,8 +5,8 @@
 
 #include "../config/config.h"
 
-int start_daemon(const struct config *config);
-int stop_daemon(const struct config *config);
-int restart_daemon(const struct config *config);
+int start_daemon(struct config *config);
+int stop_daemon(struct config *config);
+int restart_daemon(struct config *config);
 
 #endif /* ! DAEMON_H */
