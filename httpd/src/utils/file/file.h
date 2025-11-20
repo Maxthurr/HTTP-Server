0fc119f029a031cb2fe2a266946c1e7f6548b883
Maxthur <maxime.robert@epita.fr>
2025-11-20 20:30:03 +0100

    Move get_fullname from server.c to file.c for more consistent architecture

--- httpd/src/utils/file/file.h
+++ httpd/src/utils/file/file.h
@@ -3,6 +3,11 @@
 
 #include <sys/types.h>
 
+#include "../../config/config.h"
+#include "../string/string.h"
+
 off_t get_file_length(const char *path);
+struct string *get_fullname(const struct config *config,
+                            struct string *filename);
 
 #endif /* ! FILE_H */
