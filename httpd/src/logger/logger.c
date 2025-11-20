3d00ed386731a4932f40e3e4fe75dd89b9e5c753
Maxthur <maxime.robert@epita.fr>
2025-11-20 12:20:48 +0100

    Logger can now log http requests and reponses

--- httpd/src/logger/logger.c
+++ httpd/src/logger/logger.c
@@ -4,6 +4,7 @@
 #include <time.h>
 
 #include "../config/config.h"
+#include "../http/http.h"
 #include "../utils/string/string.h"
 
 static FILE *log_file = NULL;
@@ -41,6 +42,76 @@ void logger_log(const struct config *config, const char *message)
             config->servers->server_name->data, message);
 }
 
+static const char *status_to_string(enum request_status status)
+{
+    switch (status)
+    {
+    case BAD_REQUEST:
+        return "Bad Request";
+    case FORBIDDEN:
+        return "Forbidden";
+    case NOT_FOUND:
+        return "Not Found";
+    case METHOD_NOT_ALLOWED:
+        return "Method Not Allowed";
+    case UNSUPPORTED_VERSION:
+        return "HTTP Version Not Supported";
+    default:
+        return "Internal Server Error";
+    }
+}
+
+void logger_request(const struct config *config,
+                    const struct request_header *request,
+                    const struct string *client_ip)
+{
+    if (!log_file || !config->log)
+        return;
+
+    char msg[512];
+    if (request->status == OK)
+    {
+        sprintf(msg, "received %s on '%s' from %s",
+                request->method == GET ? "GET" : "HEAD",
+                request->filename ? request->filename->data : "/",
+                client_ip->data);
+    }
+    else
+    {
+        sprintf(msg, "received %s from %s", status_to_string(request->status),
+                client_ip->data);
+    }
+    logger_log(config, msg);
+}
+
+void logger_response(const struct config *config,
+                     const struct request_header *request,
+                     const struct string *client_ip)
+{
+    if (!log_file || !config->log)
+        return;
+
+    char msg[512];
+
+    if (request->status == BAD_REQUEST)
+    {
+        sprintf(msg, "responded with %d to %s", request->status,
+                client_ip->data);
+    }
+    else
+    {
+        char *method = request->method == GET ? "GET"
+            : request->method == HEAD         ? "HEAD"
+                                              : "UNKNOWN";
+
+        sprintf(msg, "responded with %d to %s for %s on '%s'", request->status,
+                client_ip->data, method,
+                request->filename ? request->filename->data : "/");
+    }
+
+    logger_log(config, msg);
+}
+
 void logger_error(const struct config *config, const char *source,
                   const char *message)
 {
@@ -48,7 +119,7 @@ void logger_error(const struct config *config, const char *source,
         return;
 
     char msg[512];
-    snprintf(msg, sizeof(msg), "An error occured in %s: %s", source, message);
+    sprintf(msg, "An error occured in %s: %s", source, message);
     logger_log(config, msg);
 }
 
--- httpd/src/logger/logger.h
+++ httpd/src/logger/logger.h
@@ -2,9 +2,16 @@
 #define LOGGER_H
 
 #include "../config/config.h"
+#include "../http/http.h"
 
 int logger_init(const struct config *config);
 void logger_log(const struct config *config, const char *message);
+void logger_request(const struct config *config,
+                    const struct request_header *request,
+                    const struct string *client_ip);
+void logger_response(const struct config *config,
+                     const struct request_header *request,
+                     const struct string *client_ip);
 void logger_error(const struct config *config, const char *source,
                   const char *message);
 void logger_destroy(void);
