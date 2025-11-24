64796a070d4ea77d2a3160c10b2489263cf1237c
Maxthur <maxime.robert@epita.fr>
2025-11-24 22:52:45 +0100

    remove get_fullname function as it was not in use anymore

--- httpd/src/utils/file/file.c
+++ httpd/src/utils/file/file.c
@@ -1,13 +1,9 @@
 #include "file.h"
 
 #include <stdbool.h>
-#include <string.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
-
-#include "../../config/config.h"
-#include "../string/string.h"
 
 off_t get_file_length(const char *path)
 {
@@ -16,23 +12,4 @@ off_t get_file_length(const char *path)
         return -1;
 
     return buffer.st_size;
-}
-
-struct string *get_fullname(const struct config *config,
-                            struct string *filename)
-{
-    struct string *fullpath = string_create(config->servers->root_dir,
-                                            strlen(config->servers->root_dir));
-
-    string_concat_str(fullpath, filename->data,
-                      filename->size - 1); // Exclude null byte
-
-    if (fullpath->size > 0 && fullpath->data[fullpath->size - 1] == '/')
-    {
-        string_concat_str(fullpath, config->servers->default_file,
-                          strlen(config->servers->default_file));
-    }
-
-    string_concat_str(fullpath, "\0", 1); // Null-terminate string
-    return fullpath;
 }
--- httpd/src/utils/file/file.h
+++ httpd/src/utils/file/file.h
@@ -3,11 +3,6 @@
 
 #include <sys/types.h>
 
-#include "../../config/config.h"
-#include "../string/string.h"
-
 off_t get_file_length(const char *path);
-struct string *get_fullname(const struct config *config,
-                            struct string *filename);
 
 #endif /* ! FILE_H */
